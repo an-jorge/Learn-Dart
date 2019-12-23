76455812a0dd42f1d2b8b37386e60249c38f4a5b
An. Jorge <an.jorge@icloud.com>
2019-12-23 22:46:48 +0100

    Hello World from Dart Lang

---
+++ Playground/Basic/strings.dart
@@ -0,0 +1,19 @@
+// Strings and Variables 
+
+
+var userName = 'Jonh';    // Variavel implicita
+var lastName = 'Flutter';
+
+String country = "Angola";    // Outra forma de declarar variaveis (Explicita)
+String city = "Luanda";
+
+
+// Conversion 
+
+var lucky = 7.toString();
+
+
+main() {
+  print("Hello ${userName} ${lastName}");
+  print(lucky);
+}
