931db100466b6ac6eda184fa9e29c3cc17c1915b
An. Jorge <an.jorge@icloud.com>
2019-12-26 16:04:36 +0100

    Strings and concatenation

--- Playground/Basic/strings.dart
+++ Playground/Basic/strings.dart
@@ -1,7 +1,7 @@
 // Strings and Variables 
 
 
-var userName = 'Jonh';    // Variavel implicita
+var userName = 'Mrs';    // Variavel implicita
 var lastName = 'Flutter';
 
 String country = "Angola";    // Outra forma de declarar variaveis (Explicita)
@@ -11,9 +11,25 @@ String city = "Luanda";
 // Conversion 
 
 var lucky = 7.toString();
+var bigCountry = 'Canada'.toUpperCase();
+
+
+// String methods
+
+var fullName = "Larry Page";
+
 
 
 main() {
-  print("Hello ${userName} ${lastName}");
+  print("Hello $userName $lastName");
   print(lucky);
+  print(bigCountry);
+
+  // String methods
+  print('${fullName.startsWith('Larry')}');
+  print("${fullName.endsWith('Age')}");
+  print('${fullName.contains('L')}');
+  print('${fullName.length == 10}');
 }
+
+
