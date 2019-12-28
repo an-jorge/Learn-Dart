2bf906a712b394ee37de8eaef2e74d8c4a5d8b3a
An. Jorge <an.jorge@icloud.com>
2019-12-28 22:20:23 +0100

    Some Conditional options

---
+++ Playground/Basic/conditions.dart
@@ -0,0 +1,24 @@
+bool freeDay = true;
+
+main() {
+
+  if (freeDay == true) {
+    print('Free time YEAH!!');
+  } else {
+    print('Ohh NOOO nooo!!!');
+  }
+
+
+// number conditional
+
+var favoriteNumber = 1;
+
+if (favoriteNumber == 10){
+  print('$favoriteNumber is my favorite number');
+} else if (favoriteNumber >= 10){
+  print('Too hight');
+} else if (favoriteNumber <= 10){
+  print('Too low');
+  }
+
+}
