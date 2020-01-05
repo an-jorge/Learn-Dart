c448a5b7f2992eb1792b3786a94d46df4396ed71
An. Jorge <an.jorge@icloud.com>
2020-01-05 08:48:22 +0100

    009 – Tabuada - Completed - Refactor

--- Playground/Challenges/009 – Tabuada.dart
+++ Playground/Challenges/009 – Tabuada.dart
@@ -4,12 +4,10 @@ Exercício Python 009: Faça um programa que leia um número Inteiro qualquer e 
 
  */
 
-var numerosTab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-var casa = 10;
+var numberTab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var decimal = 2;
 main(){
-  int numeros = null;
-  for (numeros in numerosTab) {
-    print('$casa * $numeros = ${casa * numeros}');
+  for (var number in numberTab) {
+    print('$decimal x $number = ${decimal * number}');
   }
-
 }
