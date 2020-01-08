26ceedbcf5b1b162b9fa1331bcede7a633248fc3
An. Jorge <an.jorge@icloud.com>
2020-01-08 01:11:34 +0100

    027 – Primeiro e último nome - Completed

---
+++ Playground/Challenges/027 – Primeiro e último nome.dart
@@ -0,0 +1,11 @@
+/*
+27 – Primeiro e último nome
+Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro 
+e o último nome separadamente.
+ */
+
+void main(){
+  String firstName = 'Alita';
+  String lastName = 'Houston';
+  print('Hello Mr. \n Firstname: $firstName \n Lastname: $lastName');
+}
