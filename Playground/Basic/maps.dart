e58034dd60fd00c910765b1163e797a8852bbb47
An. Jorge <an.jorge@icloud.com>
2020-01-18 23:51:37 +0100

    maps type

---
+++ Playground/Basic/maps.dart
@@ -0,0 +1,13 @@
+/*
+
+Lists and maps are commonly used collection types. In Dart, arrays are List objects. Lists and maps can be created using literals or constructors. It’s recommended to use collection literals when possible. 
+ */
+
+main() {
+ var contacts = {'name': 'Bene', 'number': 234};
+
+contacts['name'] = 'Marcos';
+
+print(contacts);
+
+}
