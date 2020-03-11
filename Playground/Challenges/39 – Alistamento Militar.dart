14178d89d6889be0b2b6bf0f11204e53e80f375e
An. Jorge <an.jorge@icloud.com>
2020-03-11 12:24:40 +0100

    Create 39 – Alistamento Militar.dart
    39 – Alistamento Militar

---
+++ Playground/Challenges/39 – Alistamento Militar.dart
@@ -0,0 +1,23 @@
+/*
+
+39 – Alistamento Militar
+Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento.
+Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+
+
+
+*/
+main() {
+  int born = 2001;
+  int date = 2020;
+  int timeLeft = date - born;
+  
+  if (timeLeft < 18) {
+    print('You are very young, come back in $timeLeft years');
+  } else if (born == 2002){
+    print('This is the time to join the military');
+  } else if (timeLeft >= 18){
+    print('The was over in ${timeLeft - 18} years');
+  }
+    
+}
