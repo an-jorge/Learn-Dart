6af6dfb4b92584361d6ed45e44f638736b49d1aa
An. Jorge <an.jorge@icloud.com>
2020-06-02 13:36:04 +0000

    chances

--- Playground/Challenges/005 - Antecessor e Sucessor.dart
+++
@@ -1,9 +0,0 @@
-/* 005: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor. */
-
-var number = 10;
-
-main() {
-  print('O número escolhido é: $number');
-  print('O atecessor é: ${number - 1}\nE o sucessor é ${number += 1}');
-}
-  
