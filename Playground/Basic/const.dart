eb41c10b578cfa3925a520aaa94511df56db5313
An. Jorge <an.jorge@icloud.com>
2020-06-18 23:33:52 +0100

    moved

--- Playground/Basic/const.dart
+++
@@ -1,6 +0,0 @@
-main() {
-  var name = 'Dart Show';
-  const year = 2011;        // Constant can't change only variables
-  print('Hello, $name');
-  print('Year:  $year');
-}
--- Playground/Basic/loopFor.dart
+++
@@ -1,5 +0,0 @@
-main(){
-  for (int x = 0; x < 10; x ++){
-    print('Hello for $xᵃ time');
-  }
-}
