d00f3268c940b4cbba4134d06804214cfbc29b10
An. Jorge <an.jorge@icloud.com>
2020-07-04 22:41:37 +0100

    simplify initialization

--- overview/smallProjects/books/classBook.dart
+++ overview/smallProjects/books/classBook.dart
@@ -1,12 +1,6 @@
 class Books {
   String title;
   int pages;
-  double cost;
-  int published;
 
-  Books({this.title, this.pages, this.cost, this.published});
-}
-
-class FavBooks extends Books {
-  FavBooks() : super();
+  Books({this.title, this.pages});
 }
